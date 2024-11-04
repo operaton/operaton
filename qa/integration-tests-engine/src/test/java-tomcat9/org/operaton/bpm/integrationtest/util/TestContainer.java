72e7911130cbcabf0e73f09cfc6e05ca28a3ddfa
Tim Zöller <tim.zoeller@lambdaschmiede.com>
2024-11-04 08:54:50 +0100

    chore: Fix license headers

--- qa/integration-tests-engine/src/test/java-tomcat9/org/operaton/bpm/integrationtest/util/TestContainer.java
+++ qa/integration-tests-engine/src/test/java-tomcat9/org/operaton/bpm/integrationtest/util/TestContainer.java
@@ -2,7 +2,7 @@
  * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH
  * under one or more contributor license agreements. See the NOTICE file
  * distributed with this work for additional information regarding copyright
- * ownership. Operaton licenses this file to you under the Apache License,
+ * ownership. Camunda licenses this file to you under the Apache License,
  * Version 2.0; you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *

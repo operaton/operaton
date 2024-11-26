6ad913631aa3c92626263260ac90c8c3e4ad7d5f
Tim Zöller <tim.zoeller@lambdaschmiede.com>
2024-11-26 21:42:05 +0100

    feat(engine): Add property for skipping locked jobs in the job executor query [#264]
    # Conflicts:
    #	engine/src/main/java/org/operaton/bpm/engine/ProcessEngineConfiguration.java

--- engine/src/test/java/org/operaton/bpm/engine/test/jobexecutor/AbstractJobExecutorAcquireJobsTest.java
+++ engine/src/test/java/org/operaton/bpm/engine/test/jobexecutor/AbstractJobExecutorAcquireJobsTest.java
@@ -44,6 +44,7 @@ public abstract class AbstractJobExecutorAcquireJobsTest {
 
   private boolean jobExecutorAcquireByDueDate;
   private boolean jobExecutorAcquireByPriority;
+  private boolean jobExecutorAcquireWithSkipLocked;
   private boolean jobExecutorPreferTimerJobs;
   private boolean jobEnsureDueDateSet;
   private Long jobExecutorPriorityRangeMin;
@@ -53,6 +54,7 @@ public abstract class AbstractJobExecutorAcquireJobsTest {
   public void saveProcessEngineConfiguration() {
     jobExecutorAcquireByDueDate = configuration.isJobExecutorAcquireByDueDate();
     jobExecutorAcquireByPriority = configuration.isJobExecutorAcquireByPriority();
+    jobExecutorAcquireWithSkipLocked = configuration.isJobExecutorAcquireWithSkipLocked();
     jobExecutorPreferTimerJobs = configuration.isJobExecutorPreferTimerJobs();
     jobEnsureDueDateSet = configuration.isEnsureJobDueDateNotNull();
     jobExecutorPriorityRangeMin = configuration.getJobExecutorPriorityRangeMin();
@@ -68,6 +70,7 @@ public abstract class AbstractJobExecutorAcquireJobsTest {
   public void restoreProcessEngineConfiguration() {
     configuration.setJobExecutorAcquireByDueDate(jobExecutorAcquireByDueDate);
     configuration.setJobExecutorAcquireByPriority(jobExecutorAcquireByPriority);
+    configuration.setJobExecutorAcquireWithSkipLocked(jobExecutorAcquireWithSkipLocked);
     configuration.setJobExecutorPreferTimerJobs(jobExecutorPreferTimerJobs);
     configuration.setEnsureJobDueDateNotNull(jobEnsureDueDateSet);
     configuration.setJobExecutorPriorityRangeMin(jobExecutorPriorityRangeMin);

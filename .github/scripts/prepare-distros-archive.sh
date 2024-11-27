2ad16a880e25b3f98e83af29d0958f3593cd89c9
Karsten Thoms <thoms@itemis.de>
2024-11-27 23:49:04 +0100

    feat(build): Provide distros as separate downloadable artifacts
    This change modifies `build.yml` and `nightly-build.yml` to produce separate artifacts attached to the build.
    
    closes #280

--- .github/scripts/prepare-distros-archive.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-TARGET_DIR=$1
-
-echo "📦 Collecting distros"
-mkdir -p $TARGET_DIR
-mv distro/sql-script/target/operaton-sql-scripts-*.tar.gz $TARGET_DIR
-mv distro/tomcat/distro/target/operaton-bpm-tomcat-*.tar.gz $TARGET_DIR

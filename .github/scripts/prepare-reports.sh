ecf45cd957a2e2506eb67b07b83337dd54f903ef
Karsten Thoms <thoms@itemis.de>
2024-12-18 21:41:25 +0100

    chore(build): Use JReleaser for nightly releases
    jreleaser.yml:
    - include 'run' distro as artifacts
    - package project reports as zip and attach as release artifacts
    - set previousTagName: 1.0.0-beta-2
    
    nightly-build.yml:
    - use jacoco-create-flag-files.sh
    - add `release` job
    - include 'distro-run'
    - make Java version selectable
    
    closes #347
    
    release.yml:
    - set JRELEASER_PRERELEASE_ENABLED: false
    - include 'distro-run'
    
    prepare-reports.sh:
    - package reports as zip
    
    jacoco-create-flag-files.sh:
    - extract shell script from nightly-build.yml
    
    build.yml:
    - use jacoco-create-flag-files.sh

--- .github/scripts/prepare-reports.sh
+++ .github/scripts/prepare-reports.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
-SLOC_FILES=$(find . -name sloc.txt | paste -sd ',' -)
 TARGET_DIR=target/reports/sloc
 
 echo "📦 Collecting sloc.txt files"
+SLOC_FILES=$(find . -name sloc.txt | paste -sd ',' -)
 mkdir -p $TARGET_DIR
 for FILE in $(echo $SLOC_FILES | tr "," "\n"); do
   TARGET_FILE_NAME=$(echo $FILE | sed 's/\.\///g' | sed 's/target\///g' | sed 's/\//_/g' )
   mv -v $FILE $TARGET_DIR/$TARGET_FILE_NAME
 done
+
+zip -rq target/project-reports.zip target/reports/*
+echo "📦 Created project-reports.zip"

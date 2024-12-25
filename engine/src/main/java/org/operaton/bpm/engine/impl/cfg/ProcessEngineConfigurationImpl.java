e0678b7d144c2d1fef6093a46fa14203e84b715c
Julian Haupt <julian@haupt.media>
2024-12-25 13:05:56 +0100

    chore(engine,parent): Bump GraalVM to 21.3.12 (#368)
    Notes
    
    - Bumps icu:icu4j to version 71.1
    
    Introduces:
    
    CustomScriptEngineManager - Class that allows to parameterise behaviour needed for a given JS engine: a) After the engines are detected from the classpath b) Before the engines are created
    
    GraalVM Warnings
    
    The commit sets the System property to disable the respective warnings introduced by bumping GraalVM to version 21.3.10.
    
    - The system property is always set when a GraalJS script engine is found on the classpath
    - The above applies by default to all distros
    
    Related-to: https://github.com/camunda/camunda-bpm-platform/issues/4299
    
    Backported commit 14a704b522 from the camunda-bpm-platform repository.
    Original author: psavidis <69160690+psavidis@users.noreply.github.com>

--- engine/src/main/java/org/operaton/bpm/engine/impl/cfg/ProcessEngineConfigurationImpl.java
+++ engine/src/main/java/org/operaton/bpm/engine/impl/cfg/ProcessEngineConfigurationImpl.java
@@ -42,7 +42,6 @@ import java.util.Properties;
 import java.util.Set;
 import java.util.concurrent.CopyOnWriteArraySet;
 import javax.naming.InitialContext;
-import javax.script.ScriptEngineManager;
 import javax.sql.DataSource;
 import org.apache.ibatis.builder.xml.XMLConfigBuilder;
 import org.apache.ibatis.datasource.pooled.PooledDataSource;
@@ -340,6 +339,7 @@ import org.operaton.bpm.engine.impl.runtime.DefaultCorrelationHandler;
 import org.operaton.bpm.engine.impl.runtime.DefaultDeserializationTypeValidator;
 import org.operaton.bpm.engine.impl.scripting.ScriptFactory;
 import org.operaton.bpm.engine.impl.scripting.engine.BeansResolverFactory;
+import org.operaton.bpm.engine.impl.scripting.engine.OperatonScriptEngineManager;
 import org.operaton.bpm.engine.impl.scripting.engine.DefaultScriptEngineResolver;
 import org.operaton.bpm.engine.impl.scripting.engine.ResolverFactory;
 import org.operaton.bpm.engine.impl.scripting.engine.ScriptBindingsFactory;
@@ -2642,7 +2642,7 @@ public abstract class ProcessEngineConfigurationImpl extends ProcessEngineConfig
       resolverFactories.add(new BeansResolverFactory());
     }
     if (scriptEngineResolver == null) {
-      scriptEngineResolver = new DefaultScriptEngineResolver(new ScriptEngineManager());
+      scriptEngineResolver = new DefaultScriptEngineResolver(new OperatonScriptEngineManager());
     }
     if (scriptingEngines == null) {
       scriptingEngines = new ScriptingEngines(new ScriptBindingsFactory(resolverFactories), scriptEngineResolver);

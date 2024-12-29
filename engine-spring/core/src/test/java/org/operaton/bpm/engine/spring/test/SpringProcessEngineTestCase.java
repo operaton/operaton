102b2a85e36784332c4536b1f7449ade7b866f99
Karsten Thoms <thoms@itemis.de>
2024-12-29 08:54:10 +0100

    refactor(tests): JUnit 5/AssertJ migration for module engine-spring
    - Copy deployment support from ProcessEngineExtension

--- engine-spring/core/src/test/java/org/operaton/bpm/engine/spring/test/SpringProcessEngineTestCase.java
+++ engine-spring/core/src/test/java/org/operaton/bpm/engine/spring/test/SpringProcessEngineTestCase.java
@@ -21,18 +21,24 @@ import org.operaton.bpm.engine.ProcessEngine;
 import org.operaton.bpm.engine.RuntimeService;
 import org.operaton.bpm.engine.TaskService;
 import org.operaton.bpm.engine.impl.cfg.ProcessEngineConfigurationImpl;
+import org.operaton.bpm.engine.impl.diagnostics.PlatformDiagnosticsRegistry;
+import org.operaton.bpm.engine.impl.test.TestHelper;
+import org.operaton.bpm.engine.impl.util.ClockUtil;
 
 import java.util.ServiceLoader;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestInfo;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.CachedIntrospectionResults;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.test.context.TestContextManager;
 import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 
 
@@ -40,6 +46,7 @@ import org.springframework.test.context.support.DependencyInjectionTestExecution
  * @author Joram Barrez
  */
 @TestExecutionListeners(DependencyInjectionTestExecutionListener.class)
+@ExtendWith(SpringExtension.class)
 public abstract class SpringProcessEngineTestCase implements ApplicationContextAware {
 
   protected ProcessEngine processEngine;
@@ -49,8 +56,8 @@ public abstract class SpringProcessEngineTestCase implements ApplicationContextA
   protected ManagementService managementService;
   protected TestContextManager testContextManager;
 
-  @Autowired
   protected ConfigurableApplicationContext applicationContext;
+  private String deploymentId;
 
   public SpringProcessEngineTestCase() {
     super();
@@ -67,17 +74,41 @@ public abstract class SpringProcessEngineTestCase implements ApplicationContextA
   }
 
   @BeforeEach
-  protected void setUp() throws Exception {
+  protected void setUp(TestInfo testInfo) throws Exception {
     processEngine = applicationContext.getBean(ProcessEngine.class);
     processEngineConfiguration = (ProcessEngineConfigurationImpl) processEngine.getProcessEngineConfiguration();
     runtimeService = processEngine.getRuntimeService();
     taskService = processEngine.getTaskService();
     managementService = processEngine.getManagementService();
+    deploymentSetUp(testInfo);
+
     testContextManager.prepareTestInstance(this); // this will initialize all dependencies
   }
 
+  private void deploymentSetUp(TestInfo testInfo) {
+    var testClass = testInfo.getTestClass().orElseThrow();
+    var testMethod = testInfo.getTestMethod().orElseThrow();
+
+    deploymentId = TestHelper.annotationDeploymentSetUp(processEngine, testClass, testMethod.getName(), null, testMethod.getParameterTypes());
+    boolean hasRequiredHistoryLevel = TestHelper.annotationRequiredHistoryLevelCheck(processEngine, testClass, testMethod.getName(), testMethod.getParameterTypes());
+    boolean hasRequiredDatabase = TestHelper.annotationRequiredDatabaseCheck(processEngine, testClass, testMethod.getName(), testMethod.getParameterTypes());
+
+    Assumptions.assumeTrue(hasRequiredHistoryLevel, "ignored because the current history level is too low");
+    Assumptions.assumeTrue(hasRequiredDatabase, "ignored because the database doesn't match the required ones");
+  }
+
   @AfterEach
-  protected void tearDown() throws Exception {
+  protected void tearDown(TestInfo testInfo) throws Exception {
+    var testClass = testInfo.getTestClass().orElseThrow();
+    var testMethod = testInfo.getTestMethod().orElseThrow();
+
+    TestHelper.annotationDeploymentTearDown(processEngine, deploymentId, testClass, testMethod.getName());
+    deploymentId = null;
+
+    TestHelper.resetIdGenerator(processEngineConfiguration);
+    ClockUtil.reset();
+    PlatformDiagnosticsRegistry.clear();
+
     testContextManager.afterTestClass();
     applicationContext.close();
     applicationContext = null;
@@ -87,6 +118,8 @@ public abstract class SpringProcessEngineTestCase implements ApplicationContextA
     taskService = null;
     managementService = null;
     testContextManager = null;
+
+
     CachedIntrospectionResults.clearClassLoader(getClass().getClassLoader());
   }
 

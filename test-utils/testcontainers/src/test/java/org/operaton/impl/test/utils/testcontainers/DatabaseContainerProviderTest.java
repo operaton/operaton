1a01bc0ed0982460d7bf27e85bb0161fe79e0e2e
Karsten Thoms <thoms@itemis.de>
2025-01-01 14:26:19 +0100

    refactor(tests): JUnit 5 migration of module test-utils/testcontainers
    related to #27

--- test-utils/testcontainers/src/test/java/org/operaton/impl/test/utils/testcontainers/DatabaseContainerProviderTest.java
+++ test-utils/testcontainers/src/test/java/org/operaton/impl/test/utils/testcontainers/DatabaseContainerProviderTest.java
@@ -16,61 +16,42 @@
  */
 package org.operaton.impl.test.utils.testcontainers;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.fail;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Arrays;
-import java.util.Collection;
 
-import org.junit.Ignore;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
-/**
- * This test should not be run on our CI, as it requires a Docker-in-Docker image to run successfully.
- */
-@Ignore
-@RunWith(Parameterized.class)
-public class DatabaseContainerProviderTest {
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
 
+@Disabled("This test should not be run on our CI, as it requires a Docker-in-Docker image to run successfully.")
+class DatabaseContainerProviderTest {
 
-  @Parameterized.Parameter(0)
-  public String jdbcUrl;
-  @Parameterized.Parameter(1)
-  public String versionStatement;
-  @Parameterized.Parameter(2)
-  public String dbVersion;
-
-  @Parameterized.Parameters(name = "Job DueDate is set: {0}")
-  public static Collection<Object[]> scenarios() {
-    return Arrays.asList(new Object[][] {
-      // The Operaton PostgreSQL 13.2 image is compatible with Testcontainers.
-      // For older versions, please use the public Docker images (DockerHub repo: postgres).
-      { "jdbc:tc:campostgresql:13.2:///process-engine", "SELECT version();", "13.2" },
-      // The current Operaton MariaDB images are compatible with Testcontainers.
-      // The username and password need to be explicitly declared.
-      { "jdbc:tc:cammariadb:10.0://localhost:3306/process-engine?user=operaton&password=operaton", "SELECT version();", "10.0" },
+  @ParameterizedTest(name = "{index} => jdbcUrl={0}, versionStatement={1}, dbVersion={2}")
+  @CsvSource({
+    // The Operaton PostgreSQL 13.2 image is compatible with Testcontainers.
+    // For older versions, please use the public Docker images (DockerHub repo: postgres).
+    "jdbc:tc:oppostgresql:13.2:///process-engine, SELECT version();, 13.2",
+    // The current Operaton MariaDB images are compatible with Testcontainers.
+    // The username and password need to be explicitly declared.
+    "jdbc:tc:opmariadb:10.0://localhost:3306/process-engine?user=operaton&password=operaton, SELECT version();, 10.0",
       // The current Operaton MySQL images are compatible with Testcontainers.
       // The username and password need to be explicitly declared.
-      { "jdbc:tc:cammysql:5.7://localhost:3306/process-engine?user=operaton&password=operaton", "SELECT version();", "5.7" },
-      { "jdbc:tc:cammysql:8.0://localhost:3306/process-engine?user=operaton&password=operaton", "SELECT version();", "8.0" },
+    "jdbc:tc:opmysql:5.7://localhost:3306/process-engine?user=operaton&password=operaton, SELECT version();, 5.7",
+    "jdbc:tc:opmysql:8.0://localhost:3306/process-engine?user=operaton&password=operaton, SELECT version();, 8.0",
       // The current Operaton SqlServer 2017/2019 images are compatible with Testcontainers.
-      { "jdbc:tc:camsqlserver:2017:///process-engine", "SELECT @@VERSION", "2017" },
-      { "jdbc:tc:camsqlserver:2019:///process-engine", "SELECT @@VERSION", "2019" },
-      // The current Operaton DB2 images are not compatible with Testcontainers.
-//      { "jdbc:tc:camdb2:11.1:///engine?user=operaton&password=operaton", "SELECT * FROM SYSIBMADM.ENV_INST_INFO;", "11.1"},
-      // The current Operaton Oracle images are not compatible with Testcontainers.
-//      { "jdbc:tc:camoracle:thin:@localhost:1521:xe?user=operaton&password=operaton", "SELECT * FROM v$version;", "18" }
-    });
-  }
-
-  @Test
-  public void testJdbcTestcontainersUrl() {
+    "jdbc:tc:opsqlserver:2017:///process-engine, SELECT @@VERSION, 2017",
+    "jdbc:tc:opsqlserver:2019:///process-engine, SELECT @@VERSION, 2019"
+    // The current Operaton DB2 images are not compatible with Testcontainers.
+    // { "jdbc:tc:opdb2:11.1:///engine?user=operaton&password=operaton", "SELECT * FROM SYSIBMADM.ENV_INST_INFO;", "11.1"},
+    // The current Operaton Oracle images are not compatible with Testcontainers.
+    // { "jdbc:tc:oporacle:thin:@localhost:1521:xe?user=operaton&password=operaton", "SELECT * FROM v$version;", "18" }
+  })
+  void testJdbcTestcontainersUrl(String jdbcUrl, String versionStatement, String dbVersion) {
     // when
     try (Connection connection = DriverManager.getConnection(jdbcUrl)) {
       connection.setAutoCommit(false);
@@ -84,5 +65,4 @@ public class DatabaseContainerProviderTest {
       fail("Testcontainers failed to spin up a Docker container: " + throwables.getMessage());
     }
   }
-
 }

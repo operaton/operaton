0a43ee8d2f97e869006b85dd86510bed6ebf9ea7
Karsten Thoms <thoms@itemis.de>
2025-03-03 15:39:14 +0100

    chore(build): Add maintenance workflow & scripts (#562)
    Adds a new workflow 'maintenance.yml' that in its first step allows to update the Slack invitation link.
    
    Added scripts to perform the update and reduce the script amount in workflow files (git commit).

---
+++ .devenv/scripts/maintenance/update-slack-link.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env sh
+if [ -z "$1" ]; then
+  echo "⚠️ SLACK_INVITATION_URL is not set. Exiting..."
+  exit 1
+fi
+
+SLACK_INVITATION_URL=$1
+EXPECTED_PREFIX="https://join.slack.com/t/operaton/shared_invite/"
+
+if [[ "$SLACK_INVITATION_URL" != $EXPECTED_PREFIX* ]]; then
+  echo "⚠️ Invalid SLACK_INVITATION_URL. Exiting..."
+  exit 1
+fi
+
+sed_inplace() {
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        sed -i '' -E "$@"
+    else
+        sed -i -E "$@"
+    fi
+}
+
+pushd $(pwd)
+cd $(git rev-parse --show-toplevel) || exit 1
+
+AFFECTED_FILES=(README.md CONTRIBUTING.md)
+
+for AFFECTED_FILE in "${AFFECTED_FILES[@]}"; do
+  echo "🔄 Updating Slack Invitation URL in $AFFECTED_FILE"
+  sed_inplace "s|${EXPECTED_PREFIX}[a-zA-Z0-9\-]+|${SLACK_INVITATION_URL}|" $AFFECTED_FILE
+done
+
+echo "✅  Done!"
+
+popd

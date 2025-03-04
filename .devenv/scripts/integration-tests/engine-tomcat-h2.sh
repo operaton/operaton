a9936c045fd5fe37f947072020eb1cb8b2fd2ade
Karsten Thoms <thoms@itemis.de>
2025-03-04 06:36:38 +0100

    remove scripts again

--- .devenv/scripts/integration-tests/engine-tomcat-h2.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-pushd > /dev/null || exit 1
-cd $(git rev-parse --show-toplevel) || exit 2
-./.devenv/scripts/integration-tests/engine-tomcat-h2-build.sh
-./.devenv/scripts/integration-tests/engine-tomcat-h2-test.sh
-popd > /dev/null || exit 1
--- .devenv/scripts/integration-tests/engine-wildfly-h2-test.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-pushd $(pwd) > /dev/null
-cd $(git rev-parse --show-toplevel) || exit 1
-./mvnw -Pengine-integration,wildfly,h2-in-memory verify -f qa
-popd > /dev/null

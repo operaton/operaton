4e50fff3559eb66a9ddc7c213c72068c0040540a
Karsten Thoms <thoms@itemis.de>
2025-03-04 06:36:38 +0100

    remove scripts again

--- .devenv/scripts/integration-tests/engine-wildfly-h2-build.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-pushd $(pwd) > /dev/null
-cd $(git rev-parse --show-toplevel) || exit 1
-./mvnw -DskipTests -Pdistro,distro-webjar,distro-wildfly,wildfly,h2-in-memory clean install
-popd > /dev/null
--- .devenv/scripts/integration-tests/engine-wildfly-h2.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-pushd $(pwd) > /dev/null
-cd $(git rev-parse --show-toplevel) || exit 1
-.devenv/scripts/integration-tests/engine-wildfly-h2-build.sh
-.devenv/scripts/integration-tests/engine-wildfly-h2-test.sh
-popd > /dev/null

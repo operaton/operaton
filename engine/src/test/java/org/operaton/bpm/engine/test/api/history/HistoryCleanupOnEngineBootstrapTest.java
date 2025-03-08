c3399897c5d7adc6c8704526d8a318af59cb8200
Karsten Thoms <thoms@itemis.de>
2025-03-08 19:21:37 +0100

    chore(tests): fix test

--- engine/src/test/java/org/operaton/bpm/engine/test/api/history/HistoryCleanupOnEngineBootstrapTest.java
+++ engine/src/test/java/org/operaton/bpm/engine/test/api/history/HistoryCleanupOnEngineBootstrapTest.java
@@ -41,6 +41,7 @@ import org.operaton.bpm.engine.runtime.Job;
 import org.operaton.bpm.engine.test.RequiredHistoryLevel;
 import org.junit.Test;
 import org.springframework.beans.PropertyBatchUpdateException;
+import org.springframework.beans.factory.BeanCreationException;
 
 /**
  * @author Nikola Koevski
@@ -179,10 +180,9 @@ public class HistoryCleanupOnEngineBootstrapTest {
     assertThatThrownBy(() -> ProcessEngineConfiguration
       .createProcessEngineConfigurationFromResource("org/operaton/bpm/engine/test/history/history-cleanup-batch-window-map-wrong-values.operaton.cfg.xml")
       .buildProcessEngine())
-    .isInstanceOf(Exception.class)
-    .hasRootCauseInstanceOf(PropertyBatchUpdateException.class)
-    .rootCause()
-    .hasMessageContaining("startTime");
+      .isInstanceOf(BeanCreationException.class)
+      .hasRootCauseInstanceOf(PropertyBatchUpdateException.class)
+      .rootCause().hasMessageContaining("startTime");
   }
 
   @Test

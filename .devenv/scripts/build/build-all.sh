89b298d9e54d79b6f11f6c6a58c6ca4570327597
Karsten Thoms <thoms@itemis.de>
2025-03-08 08:33:05 +0100

    extend profiles

--- .devenv/scripts/build/build-all.sh
+++ .devenv/scripts/build/build-all.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
-pushd $(pwd)
+pushd $(pwd) > /dev/null
 cd $(git rev-parse --show-toplevel) || exit 1
-./mvnw -Pdistro,distro-run,distro-tomcat,distro-wildfly,distro-webjar,distro-starter,distro-serverless,testcontainers h2-in-memory clean install
-popd
+./mvnw -Pdistro,distro-run,distro-tomcat,tomcat,distro-wildfly,wildfly,distro-webjar,distro-starter,distro-serverless,testcontainers,h2-in-memory \
+  clean install
+popd > /dev/null
+

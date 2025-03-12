ceb4a3b88e9ad7dcaa7460151b420fd4e857289b
Karsten Thoms <karsten.thoms@gmail.com>
2025-03-12 06:35:04 +0100

    feat: disable authentication & tenant check on test shutdown
    Deleting deployments may fail when features are enabled

--- engine/src/main/junit5/org/operaton/bpm/engine/test/junit5/ProcessEngineExtension.java
+++ engine/src/main/junit5/org/operaton/bpm/engine/test/junit5/ProcessEngineExtension.java
@@ -168,7 +168,7 @@ import org.slf4j.Logger;
  * </p>
  */
 public class ProcessEngineExtension implements TestWatcher,
-    TestInstancePostProcessor, BeforeEachCallback, AfterEachCallback, BeforeAllCallback,
+    TestInstancePostProcessor, BeforeEachCallback, AfterEachCallback,BeforeAllCallback,
     AfterAllCallback, ParameterResolver, ProcessEngineServices, ProcessEngineProvider {
 
   protected static final Logger LOG = ProcessEngineLogger.TEST_LOGGER.getLogger();
@@ -304,11 +304,11 @@ public class ProcessEngineExtension implements TestWatcher,
       processEngine.getProcessEngineConfiguration().setTenantCheckEnabled(true);
     }
 
-   TestHelper.resetIdGenerator(processEngineConfiguration);
-   ClockUtil.reset();
-   PlatformDiagnosticsRegistry.clear();
+      TestHelper.resetIdGenerator(processEngineConfiguration);
+      ClockUtil.reset();
+      PlatformDiagnosticsRegistry.clear();
 
-   for (UserOperationLogEntry logEntry : historyService.createUserOperationLogQuery().list()) {
+      for (UserOperationLogEntry logEntry : historyService.createUserOperationLogQuery().list()) {
      historyService.deleteUserOperationLogEntry(logEntry.getId());
    }
 

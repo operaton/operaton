8dc56ae6ace4d976dec4fe382020edc9fbbfb487
Karsten Thoms <karsten.thoms@gmail.com>
2025-03-22 08:41:38 +0100

    chore: Merge module webapps/assembly-jakarta
    - Merge webapps/assembly-jakarta into webapps/assembly
    
    closes #605

--- webapps/assembly/src/main/runtime/test/java/org/operaton/bpm/pa/rest/TestServlet.java
+++ webapps/assembly/src/main/runtime/test/java/org/operaton/bpm/pa/rest/TestServlet.java
@@ -23,11 +23,11 @@ import java.util.List;
 import java.util.Map;
 import java.util.logging.Logger;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 import org.operaton.bpm.BpmPlatform;
 import org.operaton.bpm.admin.impl.web.SetupResource;

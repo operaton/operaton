b9e286ad059d5daf1146da22356bd1c1b11b43d4
Arne Deutsch <adeutsch@itemis.de>
2025-04-09 05:48:42 +0200

    fix(tests): Fix flaky tests by avoid caching of engine. (#711)
    relates to #671

--- engine/src/test/java/org/operaton/bpm/engine/test/api/mgmt/metrics/AbstractMetricsTest.java
+++ engine/src/test/java/org/operaton/bpm/engine/test/api/mgmt/metrics/AbstractMetricsTest.java
@@ -39,7 +39,9 @@ import org.operaton.bpm.engine.test.junit5.ProcessEngineTestExtension;
 public abstract class AbstractMetricsTest {
 
   @RegisterExtension
-  protected static ProcessEngineExtension engineRule = ProcessEngineExtension.builder().build();
+  protected static ProcessEngineExtension engineRule = ProcessEngineExtension.builder()
+      .cacheForConfigurationResource(false)
+      .build();
   @RegisterExtension
   protected static ProcessEngineTestExtension testRule = new ProcessEngineTestExtension(engineRule);
 

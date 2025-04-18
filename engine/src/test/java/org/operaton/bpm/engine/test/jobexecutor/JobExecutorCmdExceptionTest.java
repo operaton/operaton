d7712a38f83e9090343657499e55cdb7cf6ea0e1
Karsten Thoms <thoms@itemis.de>
2025-04-18 13:04:02 +0200

    Use cached process engine

--- engine/src/test/java/org/operaton/bpm/engine/test/jobexecutor/JobExecutorCmdExceptionTest.java
+++ engine/src/test/java/org/operaton/bpm/engine/test/jobexecutor/JobExecutorCmdExceptionTest.java
@@ -47,10 +47,7 @@ import org.operaton.bpm.model.bpmn.Bpmn;
 class JobExecutorCmdExceptionTest {
 
   @RegisterExtension
-  protected static ProcessEngineExtension engineRule = ProcessEngineExtension.builder()
-      // XXX disabled caching because tests got flaky. see https://github.com/operaton/operaton/issues/671
-    .cacheForConfigurationResource(false)
-    .build();
+  protected static ProcessEngineExtension engineRule = ProcessEngineExtension.builder().build();
   @RegisterExtension
   protected static ProcessEngineTestExtension testRule = new ProcessEngineTestExtension(engineRule);
 

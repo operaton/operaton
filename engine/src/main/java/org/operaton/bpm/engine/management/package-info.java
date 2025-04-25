c740852ff2a1fa9865a05f90b1a5b281793847a6
Karsten Thoms <thoms@itemis.de>
2025-04-25 17:08:14 +0200

    chore: Replace package.html files by package-info.java
    closes #757
    related to #537

---
+++ engine/src/main/java/org/operaton/bpm/engine/management/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Classes related to the {@link org.operaton.bpm.engine.ManagementService}.
+ */
+package org.operaton.bpm.engine.management;
---
+++ engine/src/main/java/org/operaton/bpm/engine/query/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Classes related to the querying entities in the engine.
+ */
+package org.operaton.bpm.engine.query;

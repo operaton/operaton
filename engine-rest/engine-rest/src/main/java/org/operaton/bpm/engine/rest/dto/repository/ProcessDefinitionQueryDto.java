e105b99e7626651b3fca964bf840979af8123858
Andreas Klein <github@kungi.org>
2025-05-16 11:05:01 +0200

    Pretty up comments in ProcessDefinitionQueryDto.java

--- engine-rest/engine-rest/src/main/java/org/operaton/bpm/engine/rest/dto/repository/ProcessDefinitionQueryDto.java
+++ engine-rest/engine-rest/src/main/java/org/operaton/bpm/engine/rest/dto/repository/ProcessDefinitionQueryDto.java
@@ -165,7 +165,7 @@ public class ProcessDefinitionQueryDto extends AbstractQueryDto<ProcessDefinitio
   }
 
   /**
-   * @deprecated use {@link #setVersion(Integer)}
+   * @deprecated Use {@link #setVersion(Integer)} instead.
    */
   @Deprecated(forRemoval = true, since = "1.0")
   @OperatonQueryParam(value = "ver", converter = IntegerConverter.class)
@@ -179,7 +179,7 @@ public class ProcessDefinitionQueryDto extends AbstractQueryDto<ProcessDefinitio
   }
 
   /**
-   * @deprecated use {@link #setLatestVersion(Boolean)}
+   * @deprecated Use {@link #setLatestVersion(Boolean)} instead.
    */
   @Deprecated(forRemoval = true, since = "1.0")
   @OperatonQueryParam(value = "latest", converter = BooleanConverter.class)

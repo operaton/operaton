8217ff6e6b71997c46b4a1a510848c8ece7803bd
Karsten Thoms <karsten.thoms@gmail.com>
2025-06-16 05:28:34 +0200

    chore(tests): minor change

--- webapps/assembly/src/test/java/org/operaton/bpm/webapp/impl/util/HeaderExtension.java
+++ webapps/assembly/src/test/java/org/operaton/bpm/webapp/impl/util/HeaderExtension.java
@@ -6,7 +6,7 @@
  * Version 2.0; you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -136,6 +136,7 @@ public class HeaderExtension implements BeforeEachCallback, AfterEachCallback {
 
         try {
             connection.connect();
+            connection.getResponseCode(); // Trigger the connection and response
         } catch (IOException e) {
             throw new RuntimeException(e);
         }

06f87c9dd7e691ba6206ac2591c34875041afa66
Karsten Thoms <thoms@itemis.de>
2025-07-03 07:43:18 +0200

    chore(build): Perform Rolling database update tests in Integration Build (#947)
    - rename property `version.operaton.dbscheme` to `operaton.dbscheme.current.version`
    - rename property `operaton.version.previous` to `operaton.dbscheme.previous.version`
    - add script `build-and-run-database-update-tests.sh` for convenient test execution
    - Integrate execution of rolling db updates test in Integration Build workflow
    - Update test description in `qa/test-db-rolling-update/README.md`
    - Ensure that tests are executed with JUnit 4 provider

---
+++ .devenv/scripts/build/build-and-run-database-update-tests.sh
@@ -0,0 +1,90 @@
+#!/usr/bin/env bash
+
+EXECUTE_BUILD=true
+EXECUTE_TEST=true
+DATABASE="h2"
+VALID_DATABASES=("h2" "postgresql" "postgresql-xa" "mysql" "mariadb", "oracle" "db2" "sqlserver")
+
+##########################################################################
+check_valid_values() {
+  local param_name=$1
+  local value=$2
+  shift 2
+  local array=("$@")
+  for item in "${array[@]}"; do
+    if [[ "$value" == "$item" ]]; then
+      return 0
+    fi
+  done
+  echo "Error: Argument '$param_name' must be one of: [${array[*]}], but was '$value'"
+  exit 1
+}
+
+##########################################################################
+parse_args() {
+  while [ "$#" -gt 0 ]; do
+    case "$1" in
+      --db=*)
+        DATABASE="${1#*=}"
+        ;;
+      --no-build)
+        EXECUTE_BUILD=false
+        ;;
+      --no-test)
+        EXECUTE_TEST=false
+        ;;
+    esac
+    shift
+  done
+
+  check_valid_values "db" "$DATABASE" "${VALID_DATABASES[@]}"
+}
+
+run_build () {
+  echo "ℹ️ Building BOMs and engine modules"
+  echo "./mvnw install -f bom && ./mvnw install -DskipTests -am -pl distro/sql-script"
+  ./mvnw install -f bom && ./mvnw install -DskipTests -am -pl distro/sql-script
+  if [[ $? -ne 0 ]]; then
+    echo "❌ Error: Build failed"
+    popd > /dev/null
+    exit 1
+  fi
+}
+
+##########################################################################
+run_tests () {
+  PROFILES=(rolling-update $DATABASE)
+  MVN_ARGS+=(clean install)
+
+  echo "ℹ️ Running database update tests for database $DATABASE using profiles: [${PROFILES[*]}]"
+  echo "./mvnw -P$(IFS=,; echo "${PROFILES[*]}") $(echo "${MVN_ARGS[*]}") -f qa"
+  ./mvnw -P$(IFS=,; echo "${PROFILES[*]}") $(echo "${MVN_ARGS[*]}") -f qa
+  if [[ $? -ne 0 ]]; then
+    echo "❌ Error: Build failed"
+    popd > /dev/null
+    exit 1
+  else
+    echo "✅ Database update tests completed successfully"
+  fi
+}
+
+##########################################################################
+# main script
+parse_args "$@"
+
+pushd $(pwd) > /dev/null
+cd $(git rev-parse --show-toplevel) || exit 1
+
+if [[ "$EXECUTE_BUILD" == true ]]; then
+  run_build
+else
+  echo "ℹ️ Skipping build"
+fi
+
+if [[ "$EXECUTE_TEST" == true ]]; then
+  run_tests
+else
+  echo "ℹ️ Skipping tests"
+fi
+
+popd > /dev/null

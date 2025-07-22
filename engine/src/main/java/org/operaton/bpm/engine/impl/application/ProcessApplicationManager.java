c881ef8bf247cbdc81082e0c6bffaadd30ffade1
Rodrigo Neves <154440380+nevesrodrigo2@users.noreply.github.com>
2025-07-22 13:50:35 +0100

    refactor: use isEmpty() to check whether a StringBuilder is empty or not (#1004)

--- engine/src/main/java/org/operaton/bpm/engine/impl/application/ProcessApplicationManager.java
+++ engine/src/main/java/org/operaton/bpm/engine/impl/application/ProcessApplicationManager.java
@@ -270,7 +270,7 @@ public class ProcessApplicationManager {
   public String getRegistrationSummary() {
     StringBuilder builder = new StringBuilder();
     for (Entry<String, DefaultProcessApplicationRegistration> entry : registrationsByDeploymentId.entrySet()) {
-      if(builder.length()>0) {
+      if(!builder.isEmpty()) {
         builder.append(", ");
       }
       builder.append(entry.getKey());

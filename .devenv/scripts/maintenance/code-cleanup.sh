2383c42305aad0c0f09ea4510904fa53a9f8a19b
Karsten Thoms <karsten.thoms@gmail.com>
2025-09-02 00:54:33 +0200

    chore(build): Fix cleanup workflow

--- .devenv/scripts/maintenance/code-cleanup.sh
+++ .devenv/scripts/maintenance/code-cleanup.sh
@@ -2,5 +2,7 @@
 ./mvnw -U org.openrewrite.maven:rewrite-maven-plugin:run
   -Drewrite.activeRecipes=org.openrewrite.java.OrderImports \
   -Drewrite.activeStyles=org.operaton.style.OperatonJavaStyle \
-  -Drewrite.exportDatatables=true
+  -Drewrite.exportDatatables=true \
+  -Dskip.frontend.build=true \
+  -Pdistro,istro-run,distro-tomcat,distro-wildfly,distro-webjar,distro-starter,distro-serverless
 ./mvnw -U org.openrewrite.maven:rewrite-maven-plugin:run

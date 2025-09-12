camunda/camunda-bpm-platform

chore(bpmn-assertions): fix typo in the variable names (#5373)
related to #5382

test-utils/assert/core/src/main/java/org/camunda/bpm/engine/test/assertions/bpmn/ProcessInstanceAssert.java
@@ -130,11 +130,11 @@ public class ProcessInstanceAssert extends AbstractProcessAssert<ProcessInstance
     ActivityInstance activityInstanceTree = runtimeService().getActivityInstance(actual.getId());
 
     // Collect all children recursively
-    Stream <ActivityInstance> flattendActivityInstances = collectAllDecendentActivities(activityInstanceTree);
+    Stream <ActivityInstance> flattendActivityInstances = collectAllDescendantActivities(activityInstanceTree);
 
-    Stream<String> decendentActivityIdStream = flattendActivityInstances
+    Stream<String> descendantActivityIdStream = flattendActivityInstances
         .flatMap(activityInstance -> getActivityIdAndCollectTransitions(activityInstance));
-    List<String> decendentActivityIds = decendentActivityIdStream.filter(
+    List<String> descendantActivityIds = descendantActivityIdStream.filter(
         // remove the root id from the list
         activityId -> !activityId.equals(activityInstanceTree.getActivityId())
     ).collect(Collectors.toList());
@@ -142,11 +142,11 @@ public class ProcessInstanceAssert extends AbstractProcessAssert<ProcessInstance
     final String message = "Expecting %s " +
       (isWaitingAt ? "to be waiting at " + (exactly ? "exactly " : "") + "%s, ": "NOT to be waiting at %s, ") +
       "but it is actually waiting at %s.";
-    ListAssert<String> assertion = Assertions.assertThat(decendentActivityIds)
+    ListAssert<String> assertion = Assertions.assertThat(descendantActivityIds)
       .overridingErrorMessage(message,
         toString(current),
         Lists.newArrayList(activityIds),
-        decendentActivityIds);
+        descendantActivityIds);
     if (exactly) {
       if (isWaitingAt) {
         assertion.containsOnly(activityIds);
@@ -164,11 +164,11 @@ public class ProcessInstanceAssert extends AbstractProcessAssert<ProcessInstance
     return this;
   }
 
-  private Stream<ActivityInstance> collectAllDecendentActivities(ActivityInstance root) {
+  private Stream<ActivityInstance> collectAllDescendantActivities(ActivityInstance root) {
     ActivityInstance[] childActivityInstances = root.getChildActivityInstances();
     return Stream.concat(
         Stream.of(root),
-        Arrays.stream(childActivityInstances).flatMap(child -> collectAllDecendentActivities(child))
+        Arrays.stream(childActivityInstances).flatMap(child -> collectAllDescendantActivities(child))
     );
   }
 

operaton/operaton

test-utils/assert/core/src/main/java/org/operaton/bpm/engine/test/assertions/bpmn/ProcessInstanceAssert.java
@@ -130,11 +130,11 @@ public class ProcessInstanceAssert extends AbstractProcessAssert<ProcessInstance
     ActivityInstance activityInstanceTree = runtimeService().getActivityInstance(actual.getId());
 
     // Collect all children recursively
-    Stream <ActivityInstance> flattenedActivityInstances = collectAllDecendentActivities(activityInstanceTree);
+    Stream <ActivityInstance> flattenedActivityInstances = collectAllDescendantActivities(activityInstanceTree);
 
-    Stream<String> decendentActivityIdStream = flattenedActivityInstances
+    Stream<String> descendantActivityIdStream = flattenedActivityInstances
         .flatMap(this::getActivityIdAndCollectTransitions);
-    List<String> decendentActivityIds = decendentActivityIdStream.filter(
+    List<String> descendantActivityIds = descendantActivityIdStream.filter(
         // remove the root id from the list
         activityId -> !activityId.equals(activityInstanceTree.getActivityId())
     ).toList();
@@ -142,11 +142,11 @@ public class ProcessInstanceAssert extends AbstractProcessAssert<ProcessInstance
     final String message = "Expecting %s " +
       (isWaitingAt ? "to be waiting at " + (exactly ? "exactly " : "") + "%s, ": "NOT to be waiting at %s, ") +
       "but it is actually waiting at %s.";
-    ListAssert<String> assertion = Assertions.assertThat(decendentActivityIds)
+    ListAssert<String> assertion = Assertions.assertThat(descendantActivityIds)
       .overridingErrorMessage(message,
         toString(current),
         Lists.newArrayList(activityIds),
-        decendentActivityIds);
+        descendantActivityIds);
     if (exactly) {
       if (isWaitingAt) {
         assertion.containsOnly(activityIds);
@@ -164,11 +164,11 @@ public class ProcessInstanceAssert extends AbstractProcessAssert<ProcessInstance
     return this;
   }
 
-  private Stream<ActivityInstance> collectAllDecendentActivities(ActivityInstance root) {
+  private Stream<ActivityInstance> collectAllDescendantActivities(ActivityInstance root) {
     ActivityInstance[] childActivityInstances = root.getChildActivityInstances();
     return Stream.concat(
         Stream.of(root),
-        Arrays.stream(childActivityInstances).flatMap(this::collectAllDecendentActivities)
+        Arrays.stream(childActivityInstances).flatMap(this::collectAllDescendantActivities)
     );
   }
 
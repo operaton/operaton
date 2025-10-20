dc52188372129143652835ca1ec23fe101e03c34
Karsten Thoms <thoms@itemis.de>
2025-10-20 20:35:01 +0200

    feat(build): Publish REST API & Javadocs with release (#1484)
    Extend the build workflow and release by deployments of `javadoc.zip`, `rest-api.zip`.
    
    Added scripts to create the deployments.

---
+++ .devenv/scripts/build/rest-api-doc.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+# Copyright 2025 the Operaton contributors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Script to build REST API documentation using redocly
+
+API_SPEC_PATH="engine-rest/engine-rest-openapi/target/generated-sources/openapi-json/openapi.json"
+if [[ ! -f "$API_SPEC_PATH" ]]; then
+  echo "Info: API spec file not found at $API_SPEC_PATH. Generating it using Maven..."
+  ./mvnw -DskipTests -am -pl engine-rest/engine-rest-openapi verify
+fi
+
+INDEX_PAGE=engine-rest/engine-rest-openapi/src/main/redocly/index.html
+TARGET_DIR="target/rest-api"
+TARGET_ZIPFILE="$(pwd)/target/rest-api.zip"
+
+mkdir -p "$TARGET_DIR"
+cp $INDEX_PAGE $TARGET_DIR
+cp $API_SPEC_PATH $TARGET_DIR/operaton-rest-api.json
+echo "REST API documentation copied to: $TARGET_DIR"
+
+pushd $(pwd)
+cd $TARGET_DIR
+zip -qr $TARGET_ZIPFILE .
+echo "REST API documentation zipped to: $TARGET_ZIPFILE"
+popd
+echo "✅ Done!"

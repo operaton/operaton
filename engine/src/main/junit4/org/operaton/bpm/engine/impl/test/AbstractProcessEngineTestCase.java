a4d4f89d6ce8f740190426b4ce2e8b17b783010d
Karsten Thoms <karsten.thoms@gmail.com>
2025-10-28 06:18:01 +0100

    refactor(cleanup): Perform code cleanup action

--- engine/src/main/junit4/org/operaton/bpm/engine/impl/test/AbstractProcessEngineTestCase.java
+++ engine/src/main/junit4/org/operaton/bpm/engine/impl/test/AbstractProcessEngineTestCase.java
@@ -16,7 +16,10 @@
  */
 package org.operaton.bpm.engine.impl.test;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import java.util.concurrent.Callable;
 
 import junit.framework.AssertionFailedError;

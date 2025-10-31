88e53af144df06cb38f352b546d9dabf97fbbed6
Alexander Horbach <alexander.horbach@vitroconnect.com>
2025-10-31 14:36:51 +0100

    load concrete SpinFactory via ServiceLoader

--- spin/core/src/main/java/org/operaton/spin/SpinFactory.java
+++ spin/core/src/main/java/org/operaton/spin/SpinFactory.java
@@ -16,7 +16,7 @@
  */
 package org.operaton.spin;
 
-import org.operaton.spin.impl.SpinFactoryImpl;
+import java.util.ServiceLoader;
 import org.operaton.spin.spi.DataFormat;
 
 /**
@@ -31,8 +31,8 @@ public abstract class SpinFactory {
    */
   public static final SpinFactory INSTANCE;
   static {
-    // use the default implementation
-    INSTANCE = new SpinFactoryImpl();
+    INSTANCE = ServiceLoader.load(SpinFactory.class).findFirst()
+            .orElseThrow(() -> new IllegalStateException("No SpinFactory found"));
   }
 
   public abstract <T extends Spin<?>> T createSpin(Object parameter);

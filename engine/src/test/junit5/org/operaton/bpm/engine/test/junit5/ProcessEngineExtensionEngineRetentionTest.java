660b909001e31c0797d9cb5f4c2e518590e829ff
Mikhail Golubev <mikhail.golubev@posteo.de>
2025-12-05 09:49:22 +0100

    fix(engine): ProcessEngineExtension closes engine and removes references in afterAll only if a closeEngine flag is set (#1719)
    added test case
    ---------
    
    Co-authored-by: Karsten Thoms <thoms@itemis.de>

---
+++ engine/src/test/junit5/org/operaton/bpm/engine/test/junit5/ProcessEngineExtensionEngineRetentionTest.java
@@ -0,0 +1,62 @@
+package org.operaton.bpm.engine.test.junit5;
+
+import org.junit.jupiter.api.ClassOrderer;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestClassOrder;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+@TestClassOrder(ClassOrderer.OrderAnnotation.class)
+public class ProcessEngineExtensionEngineRetentionTest {
+
+    @RegisterExtension
+    static ProcessEngineExtension processEngineExtension = ProcessEngineExtension
+            .builder()
+            .randomEngineName()
+            .build();
+
+    @RegisterExtension
+    static  ProcessEngineExtension processEngineClosingExtension = ProcessEngineExtension
+            .builder()
+            .randomEngineName()
+            .closeEngineAfterAllTests()
+            .build();
+
+
+    private static String processEngineName;
+    private static String processEngineClosingExtensionName;
+
+    @Nested
+    @Order(1)
+    class ProcessEngineExtensionFirstTests {
+
+        @Test
+        void testProcessEngineExtensionInitialAvailability() {
+            processEngineName = processEngineExtension.getProcessEngine().getName();
+            processEngineClosingExtensionName = processEngineClosingExtension.getProcessEngine().getName();
+            assertNotNull(processEngineExtension.getProcessEngine());
+            assertNotNull(processEngineClosingExtension.getProcessEngine());
+        }
+    }
+
+    @Nested
+    @Order(2)
+    class ProcessEngineExtensionSecondTests {
+
+        @Test
+        void testExtensionsProcessEngineRetention() {
+            // Check that same process engine is retained between multiple test classes
+            assertNotNull(processEngineExtension.getProcessEngine());
+            assertEquals(processEngineName, processEngineExtension.getProcessEngine().getName());
+
+            // Check that process engine is recreated between multiple test classes
+            assertNotNull(processEngineClosingExtension.getProcessEngine());
+            assertNotEquals(processEngineClosingExtensionName, processEngineClosingExtension.getProcessEngine().getName());
+        }
+    }
+}

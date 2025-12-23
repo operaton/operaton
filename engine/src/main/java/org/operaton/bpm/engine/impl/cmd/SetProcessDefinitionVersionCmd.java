47d1aba89e1bc60555154372a0e996a36313cea3
Karsten Thoms <thoms@itemis.de>
2025-12-23 04:53:33 +0100

    fix SetProcessDefinitionVersionCmd and change message

--- engine/src/main/java/org/operaton/bpm/engine/impl/cmd/SetProcessDefinitionVersionCmd.java
+++ engine/src/main/java/org/operaton/bpm/engine/impl/cmd/SetProcessDefinitionVersionCmd.java
@@ -116,9 +116,8 @@ public class SetProcessDefinitionVersionCmd implements Command<Void>, Serializab
       throw new ProcessEngineException("No process instance found for id = '%s'.".formatted(processInstanceId));
     } else if (!processInstance.isProcessInstanceExecution()) {
       throw new ProcessEngineException(
-        "A process instance id is required, but the provided id '%s' points to a child execution of process instance %s."
-            .formatted(processInstanceId, processInstance.getProcessInstanceId()) +
-        "Please invoke the %s with a root execution id.".formatted(getClass().getSimpleName()));
+        "A process instance id is required, but the provided id '%s' points to a child execution of process instance '%s'. Please invoke the %s with a root execution id."
+            .formatted(processInstanceId, processInstance.getProcessInstanceId(), getClass().getSimpleName()));
     }
     ProcessDefinitionImpl currentProcessDefinitionImpl = processInstance.getProcessDefinition();
 
@@ -228,7 +227,7 @@ public class SetProcessDefinitionVersionCmd implements Command<Void>, Serializab
 
       if (newActivity == null) {
         throw new ProcessEngineException(
-          "The new process definition (key = '%s') does not contain the current activity (id = '%s') of the process instance %s."
+          "The new process definition (key = '%s') does not contain the current activity '%s' of the process instance '%s'."
               .formatted(newProcessDefinition.getKey(), activityId, processInstanceId));
         }
 

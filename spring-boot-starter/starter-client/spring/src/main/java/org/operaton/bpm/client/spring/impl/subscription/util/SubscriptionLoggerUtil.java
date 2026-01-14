c208a7013b3597d25c3f3646fe8f61adbb29b58f
Karsten Thoms <thoms@itemis.de>
2026-01-14 23:25:02 +0100

    refactor(spring-boot-starter): Refactor logger class hierarchy (#2046)
    - make LoggerUtil final
    - removed LoggerUtil from implementing BaseLogger
    - derive previous subclasses ClientLoggerUtil, SubscriptionLoggerUtil from BaseLogger

--- spring-boot-starter/starter-client/spring/src/main/java/org/operaton/bpm/client/spring/impl/subscription/util/SubscriptionLoggerUtil.java
+++ spring-boot-starter/starter-client/spring/src/main/java/org/operaton/bpm/client/spring/impl/subscription/util/SubscriptionLoggerUtil.java
@@ -16,15 +16,15 @@
  */
 package org.operaton.bpm.client.spring.impl.subscription.util;
 
+import org.operaton.commons.logging.BaseLogger;
 import org.springframework.beans.factory.config.BeanDefinition;
 
 import org.operaton.bpm.client.spring.annotation.ExternalTaskSubscription;
 import org.operaton.bpm.client.spring.exception.NotInitializedException;
 import org.operaton.bpm.client.spring.exception.NotOpenedException;
-import org.operaton.bpm.client.spring.impl.util.LoggerUtil;
 import org.operaton.bpm.client.task.ExternalTaskHandler;
 
-public class SubscriptionLoggerUtil extends LoggerUtil {
+public final class SubscriptionLoggerUtil extends BaseLogger {
 
   public void handlerBeansFound(Class<ExternalTaskHandler> externalTaskHandlerClass,
                                 String[] beanNamesForType) {

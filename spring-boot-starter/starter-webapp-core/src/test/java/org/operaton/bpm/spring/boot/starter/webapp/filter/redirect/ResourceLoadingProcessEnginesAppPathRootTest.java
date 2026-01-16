01281d16c8213e4c9da67a57d47f4a6ad65f7789
Karsten Thoms <karsten.thoms@gmail.com>
2026-01-16 16:49:53 +0100

    refactor(spring-boot-starter): Replace usage of commons-io by Spring API

--- spring-boot-starter/starter-webapp-core/src/test/java/org/operaton/bpm/spring/boot/starter/webapp/filter/redirect/ResourceLoadingProcessEnginesAppPathRootTest.java
+++ spring-boot-starter/starter-webapp-core/src/test/java/org/operaton/bpm/spring/boot/starter/webapp/filter/redirect/ResourceLoadingProcessEnginesAppPathRootTest.java
@@ -18,7 +18,6 @@ package org.operaton.bpm.spring.boot.starter.webapp.filter.redirect;
 
 import java.net.HttpURLConnection;
 
-import org.apache.commons.io.IOUtils;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -27,6 +26,7 @@ import org.springframework.test.annotation.DirtiesContext;
 
 import org.operaton.bpm.spring.boot.starter.webapp.filter.util.FilterTestApp;
 import org.operaton.bpm.spring.boot.starter.webapp.filter.util.HttpClientExtension;
+import org.springframework.util.StreamUtils;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -53,7 +53,7 @@ class ResourceLoadingProcessEnginesAppPathRootTest {
 
     // when
     // get content returned by the request
-    String body = IOUtils.toString(con.getInputStream(), UTF_8);
+    String body = StreamUtils.copyToString(con.getInputStream(), UTF_8);
 
     // then
     assertThat(con.getResponseCode()).isEqualTo(200);

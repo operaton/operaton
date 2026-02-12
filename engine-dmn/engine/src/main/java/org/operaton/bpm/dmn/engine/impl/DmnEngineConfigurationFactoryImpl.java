58c0addbf4fe709d23cce6139d0ede56526a8ce8
Karsten Thoms <thoms@itemis.de>
2026-02-12 20:57:00 +0100

    Update engine-dmn/engine/src/main/java/org/operaton/bpm/dmn/engine/impl/DmnEngineConfigurationFactoryImpl.java

--- engine-dmn/engine/src/main/java/org/operaton/bpm/dmn/engine/impl/DmnEngineConfigurationFactoryImpl.java
+++ engine-dmn/engine/src/main/java/org/operaton/bpm/dmn/engine/impl/DmnEngineConfigurationFactoryImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2025 the Operaton contributors.
+ * Copyright 2026 the Operaton contributors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.

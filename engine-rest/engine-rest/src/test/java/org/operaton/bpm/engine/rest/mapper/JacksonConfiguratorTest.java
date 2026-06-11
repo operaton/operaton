5ab6ad00c33902a376f694a301a36dc740e8c9a5
Julian Haupt <julian@haupt.media>
2026-06-11 18:16:42 +0200

    chore(engine-rest): preserve CIBSeven copyright headers

--- engine-rest/engine-rest/src/test/java/org/operaton/bpm/engine/rest/mapper/JacksonConfiguratorTest.java
+++ engine-rest/engine-rest/src/test/java/org/operaton/bpm/engine/rest/mapper/JacksonConfiguratorTest.java
@@ -1,5 +1,10 @@
 /*
- * Copyright 2026 the Operaton contributors.
+ * Copyright CIB software GmbH and/or licensed to CIB software GmbH
+ * under one or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership. CIB software licenses this file to you under the Apache License,
+ * Version 2.0; you may not use this file except in compliance with the License.
+ * Modifications Copyright 2026 the Operaton contributors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.

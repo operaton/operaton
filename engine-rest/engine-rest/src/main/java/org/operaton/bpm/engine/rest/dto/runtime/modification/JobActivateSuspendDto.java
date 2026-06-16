04edeaf78403b7dfecddf89403fcdab2d0926da4
Julian Haupt <julian@haupt.media>
2026-06-16 17:14:39 +0200

    chore: preserve Fluxnova headers

--- engine-rest/engine-rest/src/main/java/org/operaton/bpm/engine/rest/dto/runtime/modification/JobActivateSuspendDto.java
+++ engine-rest/engine-rest/src/main/java/org/operaton/bpm/engine/rest/dto/runtime/modification/JobActivateSuspendDto.java
@@ -1,17 +1,12 @@
 /*
- * Copyright 2026 the Operaton contributors.
+ * Copyright 2025 FINOS
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at:
+ * The source files in this repository are made available under the Apache License Version 2.0.
  *
- *     https://www.apache.org/licenses/LICENSE-2.0
+ * SPDX-License-Identifier: Apache-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Fluxnova uses and includes third-party dependencies published under various licenses.
+ * By downloading and using Fluxnova artifacts, you agree to their terms and conditions.
  */
 package org.operaton.bpm.engine.rest.dto.runtime.modification;
 

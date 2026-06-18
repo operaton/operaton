c13f0b514404409002cae8a47652ceb3171504aa
Julian Haupt <julian@haupt.media>
2026-06-18 16:43:08 +0200

    chore: use Operaton license headers
    Replace FINOS headers on newly added ad-hoc subprocess files with the standard Operaton contributors Apache 2.0 header.

--- qa/integration-tests-engine/src/test/java/org/operaton/bpm/integrationtest/functional/subprocess/AdHocSubProcessIntegrationTest.java
+++ qa/integration-tests-engine/src/test/java/org/operaton/bpm/integrationtest/functional/subprocess/AdHocSubProcessIntegrationTest.java
@@ -1,9 +1,9 @@
 /*
- * Copyright 2026 FINOS
+ * Copyright 2026 the Operaton contributors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * You may obtain a copy of the License at:
  *
  *     https://www.apache.org/licenses/LICENSE-2.0
  *
